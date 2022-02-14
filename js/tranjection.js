3d2dbb4d78e2f7a92b9bdaeb7ad2839f0388b4b2
imtanvir <tanvirparvej101@gmail.com>
2022-02-14 18:01:23 +0600

    added all file of the project.

---
+++ js/tranjection.js
@@ -0,0 +1,34 @@
+// function for all work 
+const bankingFunction = function () {
+    updateBalenceInInner = document.getElementById("total-current-money-value");
+    currentBalence = parseFloat(updateBalenceInInner.innerText);
+    return {
+        elementIdfinder: function (moneyValue) {
+            const moneyVal = document.getElementById(moneyValue);
+            return moneyVal;
+        },
+        valDel: function (inputId) {
+            document.getElementById(inputId).value = '';
+        }
+    }
+}
+// deposit code 
+bankingFunction().elementIdfinder('enter-deposit').addEventListener('click', function () {
+            const oldDepoAmount = parseFloat(bankingFunction().elementIdfinder('deposit-money-value').innerText);
+            const newDepoAmount = parseFloat(bankingFunction().elementIdfinder('deposit-amount-input-value').value);
+            const totalDeposit = (newDepoAmount + oldDepoAmount);
+            bankingFunction().elementIdfinder('deposit-money-value').innerText = totalDeposit;
+            currentBalence += newDepoAmount;
+            updateBalenceInInner.innerText = currentBalence;
+            bankingFunction().valDel('deposit-amount-input-value');
+        });
+// widraw code 
+bankingFunction().elementIdfinder('enter-widraw').addEventListener('click', function () {
+            const oldwidraw = parseFloat(bankingFunction().elementIdfinder('widraw-money-value').innerText);
+            const newWidraw = parseFloat(bankingFunction().elementIdfinder('widraw-amount-input-value').value);
+            const totalWidraw = (newWidraw + oldwidraw);
+            bankingFunction().elementIdfinder('widraw-money-value').innerText = totalWidraw;
+            currentBalence -= newWidraw;
+            updateBalenceInInner.innerText = currentBalence;
+            bankingFunction().valDel('widraw-amount-input-value');
+        });
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+    './login.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
